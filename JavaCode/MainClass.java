db0f8eb543e64facee565157d816b5efc9fce281
Alex Winkel <Alex@FamWinkel.nl>
2025-05-17 11:40:56 +0200

    New class introduced

--- JavaCode/MainClass.java
+++ JavaCode/MainClass.java
@@ -2,7 +2,7 @@
 
 public class MainClass {
     public static void main(String[] args) {
-       System.out.println("hello Alex");
+       System.out.println("hello Alex the greatest");
        ServiceClass serviceClass = new ServiceClass();
        serviceClass.text();
     }
